e57b7f219871dd9edf86918cecf7425e9f7784b2
R.V.Rajagopalan <rajagopalan602@gmail.com>
2022-10-12 03:17:41 +0530

    added redux toolkit in project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { store } from '../store'
+import { Provider } from 'react-redux'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <Head>
-        <title>Amrita Internship - CIR</title>
-        <meta name="description" content="Amrita Students can submit their Internship detail and get approval from CIR online" />
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   )
 }
 
