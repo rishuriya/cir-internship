858b9a518b802c3c29d1852586b3e00d2ee5c1f4
R.V.Rajagopalan <rajagopalan602@gmail.com>
2022-10-18 18:32:52 +0530

    admin dashboard refactored, and modal added

--- components/admin/InternshipCard.tsx
+++ components/admin/InternshipCard.tsx
@@ -1,11 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 import { AiOutlineCheck, AiOutlineDown, AiOutlineUp, AiOutlineClose, AiOutlineDownload, AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+// modal
+import { Dialog, Transition } from '@headlessui/react'
 
 
 export default function InternshipCard({ internship }) {
     const [student, setStudent] = useState(null);
     const [loading, setLoading] = useState(true);
     const [showDetails, setShowDetails] = useState(false);
+
+    //modal
+    let [isOpen, setIsOpen] = useState(false)
+    let [approve, setApprove] = useState("")
+
+    function handleApprove() {
+        setIsOpen(true)
+        setApprove("approve")
+    }
+
+    function handleDisapprove() {
+        setIsOpen(true)
+        setApprove("disapprove")
+    }
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
 
     useEffect(() => {
         const userObject = {
@@ -41,6 +66,91 @@ export default function InternshipCard({ internship }) {
         <>
             {(!loading) ? (
                 <>
+                    {/* modal */}
+                    <Transition appear show={isOpen} as={Fragment}>
+                        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="fixed inset-0 bg-black bg-opacity-25" />
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 overflow-y-auto">
+                                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0 scale-95"
+                                        enterTo="opacity-100 scale-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100 scale-100"
+                                        leaveTo="opacity-0 scale-95"
+                                    >
+                                        <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                            <Dialog.Title
+                                                as="h3"
+                                                className="text-lg font-medium leading-6 text-gray-900"
+                                            >
+                                                {`Are you sure you want to ${approve} this internship?`}
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">
+                                                    {approve === "approve" ? "This will autogenerate the letter for the student" : "Kindly provide remarks stating the reason for disapproval"}
+                                                </p>
+                                            </div>
+
+                                            <div className="mt-4">
+                                                {approve === "approve" ? (
+                                                    <>
+                                                        <div className='flex justify-around'>
+                                                            <button
+                                                                onClick={closeModal}
+                                                                className='bg-green-400 border rounded-lg p-3'>
+                                                                Yes
+                                                            </button>
+                                                            <button
+                                                                onClick={closeModal}
+                                                                className='bg-red-300 border rounded-lg p-3'>
+                                                                No
+                                                            </button>
+                                                        </div>
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <div className='flex justify-around'>
+                                                            <form>
+                                                                <label>
+                                                                    {/* Remarks: */}
+                                                                    {/* <input type="text" name="name" className='border border-black' placeholder='Remarks' /> */}
+                                                                    <textarea className='border border-black p-2' id="story" name="story"
+                                                                        rows={3} cols={33}>
+                                                                    </textarea>
+                                                                </label>
+                                                                <br />
+                                                                <br />
+                                                                <div className='flex justify-center bg-primary text-white border rounded-lg'>
+                                                                    <label>
+                                                                        <input type="submit" value="Submit" />
+                                                                    </label>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </>
+                                                )
+                                                }
+                                            </div>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
+                            </div>
+                        </Dialog>
+                    </Transition>
                     <tr className=' mx-auto my-8 max-w-md md:max-w-7xl rounded-xl shadow-md border-gray-600 border-0  p-10 '>
                         <td>
                             {showDetails ? (
@@ -70,13 +180,13 @@ export default function InternshipCard({ internship }) {
 
                         <td className='px-6 py-4 whitespace-nowrap text-sm font-medium'>
                             <div className='flex flex-row justify-center flex-nowrap ml-auto mr-5 mt-3 sm:mt-5 text-center'>
-                                <button className="flex bg-green-400 rounded-2xl py-1 px-2 flex-row mx-3">
-                                    <AiOutlineCheck className='fill-green-700' size={26} /> 
-                                    {/* <span>Approve</span> */}
+                                <button onClick={handleApprove} className="flex bg-green-400 rounded-2xl py-1 px-2 flex-row mx-3 cursor-pointer">
+                                    <AiOutlineCheck className='fill-green-700' size={26} />
+                                    <span>Approve</span>
                                 </button>
-                                <button className="flex bg-red-300 rounded-2xl py-1 px-2  flex-row mx-3">
-                                    <AiOutlineClose className='fill-red-700' size={26} /> 
-                                    {/* <span>Decline</span> */}
+                                <button onClick={handleDisapprove} className="flex bg-red-300 rounded-2xl py-1 px-2 flex-row mx-3 cursor-pointer">
+                                    <AiOutlineClose className='fill-red-700' size={26} />
+                                    <span>Decline</span>
                                 </button>
                             </div>
                         </td>
