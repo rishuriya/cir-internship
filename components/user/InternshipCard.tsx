085876791089c9d53cf2409d6582c85428357403
Anshuman <anshuman8swain@gmail.com>
2022-10-24 11:09:05 +0530

    InternshipCard: loader added

--- components/user/InternshipCard.tsx
+++ components/user/InternshipCard.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { useEffect,useState } from "react";
 import {AiOutlineDownload} from 'react-icons/ai'
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useRouter } from "next/router";
 export default function InternshipCard({id}) {
 
     const [internship, setInternship] = useState([]);
+    const [loading, setLoading] = useState(true);
     const router = useRouter();
 
     useEffect(()=>{
@@ -20,6 +22,7 @@ export default function InternshipCard({id}) {
                 if(internship_data){
                     setInternship(internship_data);
                 }
+                setLoading(false);
             });
     },[])
 
@@ -69,38 +72,46 @@ export default function InternshipCard({id}) {
 
 
     return (
-        <>
-            {(internship!==null)?<div>
-                <div className="max-w-2xl px-8 py-4 mx-auto rounded-lg shadow-lg" style={{ cursor: "auto" }}>
-                    <div className="flex items-center justify-between">
-                        <div className="font-medium text-lg my-2">{internship["company_name"]}</div>
-                        {/* <span className="text-sm font-light text-gray-800">Posted on {internship["company_name"]}</span> */}
-                        {/* <p id="status" className={`px-3 py-1 text-sm font-bold transition-colors duration-200 transform rounded`}>{props.status}</p> */}
-                        {handleStatus(internship["approved"])}
+        <>{loading ? 
+            <div className="flex justify-center items-center h-96">
+             <AiOutlineLoading3Quarters
+                className="fill-primary animate-spin my-4 mx-auto"
+                size={36}/>
+            </div>
+        : ((internship!==null)?
+            <div>
+              <div className="max-w-2xl px-8 py-4 mx-auto rounded-lg shadow-lg" style={{ cursor: "auto" }}>
+                <div className="flex items-center justify-between">
+                    <div className="font-medium text-lg my-2">{internship["company_name"]}</div>
+                    {handleStatus(internship["approved"])}
+                </div>
+                <div className="mt-1">
+                    <Link href="/" className="text-2xl font-bold text-gray-700 hover:underline"><>{internship["company_website"]}</></Link>
+                    <p className="mt-1">Dates : {handleDate(internship["internship_start_date"],internship["internship_end_date"])}</p>
+                    <p className="mt-1">{internship["internship_mode"]} - {internship["training_type"]}</p>
+                </div>
+                <div className="flex justify-between">
+                    {internship["approved"] === "Disapproved"?<div className="my-3">
+                        <p className="mr-5 bg-red-200/60 px-2 py-1 rounded-xl"><span className="underline text-lg">Remarks</span> : {internship["admin_remarks"]}</p>
                     </div>
-                    <div className="mt-1">
-                        <Link href="/" className="text-2xl font-bold text-gray-700 hover:underline"><>{internship["company_website"]}</></Link>
-                        <p className="mt-1">Dates : {handleDate(internship["internship_start_date"],internship["internship_end_date"])}</p>
-                        <p className="mt-1">{internship["internship_mode"]} - {internship["training_type"]}</p>
-                    </div>
-                    <div className="flex justify-between">
-                        {internship["approved"] === "Disapproved"?<div className="my-3">
-                            <p className="mr-5 bg-red-200/60 px-2 py-1 rounded-xl"><span className="underline text-lg">Remarks</span> : {internship["admin_remarks"]}</p>
-                        </div>
-                        :<div></div>}
-                        <div>
-                            {internship["approved"] === "Approved"?<button onClick={(e)=> handleletter(e,internship["_id"],internship["approved"])}>
-                                <div className="flex flex-row">
-                                    <p className="text-sm mx-2 mt-1">Download Letter</p>
-                                    <AiOutlineDownload className="fill-black " size={28}/>
-                                </div>
-                            </button>:
-                             <AiOutlineDownload className="fill-gray-300 " size={26}/>
-                            }
-                        </div>
+                    :<div></div>}
+                    <div>
+                        {internship["approved"] === "Approved"?<button onClick={(e)=> handleletter(e,internship["_id"],internship["approved"])}>
+                            <div className="flex flex-row">
+                                <p className="text-sm mx-2 mt-1">Download Letter</p>
+                                <AiOutlineDownload className="fill-black " size={28}/>
+                            </div>
+                        </button>:
+                         <AiOutlineDownload className="fill-gray-300 " size={26}/>
+                        }
                     </div>
                 </div>
-            </div>:<></>}
+            </div>
+        </div>
+        :<></>
+        )
+     }
+           
         </>
     )
 }
