27e131f50a62c275ff597df46541857ab57c6534
rakshith6404 <rakshith6404@gmail.com>
2022-11-08 20:23:18 +0530

    added details button

--- components/admin/InternshipList.tsx
+++ components/admin/InternshipList.tsx
@@ -11,7 +11,7 @@ const tableColumns = [
     accessor: 'name'
   },
   {
-    Header: 'Roll',
+    Header: 'Roll Number',
     accessor: 'roll'
   },
   {
@@ -58,6 +58,12 @@ function InternshipList() {
       setLoading(false);
     }
   }, [internships])
+  
+  function StudentDetails(){
+    console.log("Hello world these are the students applied for Internship")
+    console.log()
+
+  }
 
 
   return (
@@ -98,6 +104,7 @@ function InternshipList() {
                   {
                     row.cells.map((cell) => {
                       return (
+                        
                         <>
                           <td
                             key={i}
@@ -109,10 +116,18 @@ function InternshipList() {
                       )
                     })
                   }
+                  <button 
+                  className="ml-2 inline-flex items-center justify-center whitespace-nowrap 
+                  rounded-md border border-transparent bg-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-pink-900" 
+                  onClick={StudentDetails}>
+                    Details
+                    </button>
+
                 </tr>
               }
               )
               }
+              
           </tbody>
         </table>
       </div>
