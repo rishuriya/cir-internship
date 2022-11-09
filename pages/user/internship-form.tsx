535ad6a4bf5ac9e80cf1ef6fc86095a34aa2b012
R.V.Rajagopalan <rajagopalan602@gmail.com>
2022-11-09 22:13:56 +0530

    minor tag error

--- pages/user/internship-form.tsx
+++ pages/user/internship-form.tsx
@@ -238,7 +238,7 @@ function InternshipForm() {
               </div>
             </div>
 
-          <div className="flex flex-wrap -mx-3 mb-6">
+          {/* <div className="flex flex-wrap -mx-3 mb-6"> */}
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-xs font-bold mb-2 text-white" htmlFor="email">
                 Email
