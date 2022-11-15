710cf31be39118c3f8c1baccff244f8dda2911ed
rakshith6404 <rakshith6404@gmail.com>
2022-11-15 16:34:39 +0530

    added editable

--- pages/user/internship-form.tsx
+++ pages/user/internship-form.tsx
@@ -409,7 +409,7 @@ function InternshipForm() {
                   Year Of Joining
                 </label>
                 <div className="relative">
-                  <input required className="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="year_of_joining" id="grid-joining-year" defaultValue={username["year_of_joining"]} readOnly>
+                  <input required className="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="year_of_joining" id="grid-joining-year" defaultValue={username["year_of_joining"]}>
                   </input>
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
