f1d8d581bb18592790489882a37d9a77af84d7fc
rakshith6404 <rakshith6404@gmail.com>
2022-11-26 18:53:18 +0530

    added approve and disapprove

--- components/admin/TableDashboard.tsx
+++ components/admin/TableDashboard.tsx
@@ -8,6 +8,7 @@ import { useTable, useGlobalFilter, useFilters, usePagination } from "react-tabl
 
 import GlobalFilter from "./GlobalFilter";
 import { ColumnFilter } from "./ColumnFilter";
+import { MdRowing } from "react-icons/md";
 
 const tableColumns = [
   {
@@ -136,6 +137,7 @@ export default function TableDashboard() {
         <InternshipDetailsModal
           closeModal={setOpenModal}
           info={StudentDetail}
+          setIsDone={setIsDone}
         />
       )}
       {(!empty && loading===false)?<div className="table max-w-5xl md:max-w-7xl mx-auto">
@@ -188,11 +190,12 @@ export default function TableDashboard() {
                         <td
                         onClick={() => StudentDetails(row)}
                           key={i}
-                          className="p-4 text-center cursor-pointer"  
+                          className="p-4 text-center cursor-pointer hover:bg-slate-200"  
                           {...cell.getCellProps()}
                         >
                           {cell.render("Cell")}
                         </td>
+                        
                       </>
                     );
                   })}
@@ -209,6 +212,7 @@ export default function TableDashboard() {
                     internship={row.original}
                     isApproved={false}
                     setIsDone={setIsDone}
+                    showModal={false}
                   />
                 </tr>
               );
