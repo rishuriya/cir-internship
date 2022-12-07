c445019d16de2face810a132e986290f836f211d
Anshuman <anshuman8swain@gmail.com>
2022-12-07 18:15:51 +0530

    feat:admin course addition modal

--- pages/user/internship-form.tsx
+++ pages/user/internship-form.tsx
@@ -391,9 +391,6 @@ function InternshipForm() {
                   Present Semester
                 </label>
                 <div className="relative">
-                  {/* <input required className="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 
-                  rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="sem" id="grid-sem" defaultValue={username["semester"]} readOnly>
-                  </input> */}
                   {
                     <select
                       required className="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none 
