3a7354c8f648c4b1af312875d5fd8a8dd5167bec
rakshith6404 <rakshith6404@gmail.com>
2023-01-08 14:36:39 +0530

    added setting to center

--- pages/admin/settings.tsx
+++ pages/admin/settings.tsx
@@ -75,8 +75,8 @@ function ApprovedInternships() {
       <div className='mx-[1px] lg:pl-[18vw]'>
 
       <SideNav/>
-        <div className='max-w-7xl px-4 sm:px-6 lg:w-[80vw] lg:mx-auto'>
-            <div className='my-3 mx-2 font-semibold text-2xl text-center'>
+      <div className='max-w-7xl px-4 sm:px-6 lg:w-[80vw] lg:mx-auto '>
+        <div className='my-3 mx-2 font-semibold text-2xl text-center'> 
                 Administrative Portal
             </div>
             <div className='font-medium mx-5 my-5 text-xl'>
